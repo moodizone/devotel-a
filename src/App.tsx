7a43a10f0f2d84dfdb0ea04dec1802d37e6b3724
moodi <moodizone@gmail.com>
2025-03-08 00:14:23 +0330

    🧱 add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import AppLayout from "@/layout";
+import { ThemeProvider } from "@/hoc/theme/provider";
+import NotFound from "@/pages/not-found";
+
 function App() {
-  return <div className="p-3 m-3 bg-red-200 text-lg">1</div>;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<div>applications</div>} />
+            <Route path="/:id" element={<div>app details</div>} />
+            <Route path="/forms" element={<div>form</div>} />
+            <Route path="/forms/:id" element={<div>forms details</div>} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/layout/index.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Separator } from "@radix-ui/react-separator";
+import { AppSidebar } from "./sidebar";
+import { endLoadingState } from "@/utils/spinner";
+import { Outlet } from "react-router";
+
+function AppLayout() {
+  React.useEffect(() => {
+    endLoadingState();
+  }, []);
+
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          <Outlet />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
+
+export default AppLayout;
