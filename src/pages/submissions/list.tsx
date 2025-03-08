a429dfabdbe936f27c0c23fdf2d89d3fc4f046d9
moodi <moodizone@gmail.com>
2025-03-08 17:26:53 +0330

    🚧 add submissions list

--- src/pages/submissions/list.tsx
+++ src/pages/submissions/list.tsx
@@ -1,10 +1,27 @@
+import * as React from "react";
+import { ColumnDef } from "@tanstack/react-table";
+
 import { useGerSubmissions } from "@/hooks/queries";
+import { DataTable } from "@/pages/submissions/table";
+import { Submission } from "@/services/type";
 
 function SubmissionsList() {
   const { data } = useGerSubmissions();
-  console.log(data);
+  const columns: ColumnDef<Record<keyof Submission, unknown>>[] =
+    React.useMemo(() => {
+      if (data.columns) {
+        return data.columns.map((key) => ({
+          accessorKey: key,
+          header: () => <span className="capitalize">{key}</span>,
+          cell: ({ row }) => (
+            <div className="w-[80px]">{row.getValue(key)}</div>
+          ),
+        }));
+      }
+      return [];
+    }, [data.columns]);
 
-  return <div>subs content goes here</div>;
+  return <DataTable data={data.data ?? []} columns={columns} />;
 }
 
 export default SubmissionsList;
---
+++ src/pages/submissions/table/data-table-toolbar.tsx
@@ -0,0 +1,56 @@
+import { Cross2Icon } from "@radix-ui/react-icons";
+import { Table } from "@tanstack/react-table";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { DataTableViewOptions } from "./data-table-view-options";
+import { DataTableFacetedFilter } from "./data-table-faceted-filter";
+import { Gender } from "@/services/type";
+
+interface DataTableToolbarProps<TData> {
+  table: Table<TData>;
+}
+
+export function DataTableToolbar<TData>({
+  table,
+}: DataTableToolbarProps<TData>) {
+  const isFiltered = table.getState().columnFilters.length > 0;
+
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex flex-1 flex-wrap items-center gap-2">
+        <Input
+          placeholder="Filter by name..."
+          value={
+            (table.getColumn("Full Name")?.getFilterValue() as string) ?? ""
+          }
+          onChange={(event) =>
+            table.getColumn("Full Name")?.setFilterValue(event.target.value)
+          }
+          className="h-8 w-[150px] lg:w-[250px]"
+        />
+        {table.getColumn("Gender") && (
+          <DataTableFacetedFilter
+            column={table.getColumn("Gender")}
+            title="Gender"
+            options={[
+              { label: Gender.female, value: Gender.female },
+              { label: Gender.male, value: Gender.male },
+            ]}
+          />
+        )}
+        {isFiltered && (
+          <Button
+            variant="ghost"
+            onClick={() => table.resetColumnFilters()}
+            className="h-8 px-2 lg:px-3"
+          >
+            {"Reset"}
+            <Cross2Icon className="ms-2 h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <DataTableViewOptions table={table} />
+    </div>
+  );
+}
