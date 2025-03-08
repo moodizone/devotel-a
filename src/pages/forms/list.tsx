c627e66428b71a210ace4fe7295862c13d5bfd07
moodi <moodizone@gmail.com>
2025-03-08 21:16:03 +0330

    🚧 add form list page

--- src/pages/forms/list.tsx
+++ src/pages/forms/list.tsx
@@ -1,10 +1,34 @@
+import { Link } from "react-router";
+
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { useGetForms } from "@/hooks/queries";
+import { ROUTES } from "@/router/routes";
 
 function FormsList() {
   const { data } = useGetForms();
-  console.log(data);
 
-  return <div>forms content goes here</div>;
+  return (
+    <div className="flex flex-wrap gap-4">
+      {data?.map(({ formId, title, fields }) => {
+        const to = ROUTES.forms.details.replace(":id", formId);
+        return (
+          <Link to={to} key={formId}>
+            <Card>
+              <CardHeader>
+                <CardTitle>{title}</CardTitle>
+                <CardDescription>{`${fields.length} fields`}</CardDescription>
+              </CardHeader>
+            </Card>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
 
 export default FormsList;
